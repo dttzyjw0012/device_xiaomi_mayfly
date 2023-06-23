f41b2e92d4db753d3cdf98debae52d74952f80d7
YHC <YHC@yhc.com>
2023-06-23 12:08:51 +0800

    Add device makefile.

--- aosp_mayfly.mk
+++ aosp_mayfly.mk
@@ -4,12 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# TODO
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# TODO
 # Inherit from mayfly device
 $(call inherit-product, device/xiaomi/mayfly/device.mk)
 
